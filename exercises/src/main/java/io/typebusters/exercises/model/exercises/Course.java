13dcbeb4528f71d041c73d183ac554820cbee2be
Georgich88 <georgeisaev@gmail.com>
2020-08-01 10:35:43 +0700

    Course model: interface, validation and serializable

--- exercises/src/main/java/io/typebusters/exercises/model/exercises/Course.java
+++ exercises/src/main/java/io/typebusters/exercises/model/exercises/Course.java
@@ -1,15 +1,21 @@
 package io.typebusters.exercises.model.exercises;
 
+import java.io.Serializable;
 import java.util.List;
+
+import javax.validation.constraints.NotBlank;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-public class Course {
+public class Course implements CourseUnderStudy, Serializable {
 	
+	private static final long serialVersionUID = 5833164416273240874L;
+
 	@Id
+	@NotBlank
 	private String id;
 	
-	List<Exercise> exercises;
+	List<ExerciseUnderStudy> exercises;
 
 }
