99182aa909c5070c6ad738bc5852bc937da6e959
Georgich88 <georgeisaev@gmail.com>
2020-08-01 14:09:00 +0700

    Model: course and exercise. Interfaces and serializable.

--- exercises/src/main/java/io/typebusters/exercises/model/exercises/Exercise.java
+++ exercises/src/main/java/io/typebusters/exercises/model/exercises/Exercise.java
@@ -1,5 +1,11 @@
 package io.typebusters.exercises.model.exercises;
 
-public class Exercise {
+import java.io.Serializable;
+
+public class Exercise implements ExerciseUnderStudy, Serializable {
+
+	private static final long serialVersionUID = -3677106373186522494L;
+	
+	
 
 }
---
+++ exercises/src/main/java/io/typebusters/exercises/service/exercises/CourseService.java
@@ -0,0 +1,8 @@
+package io.typebusters.exercises.service.exercises;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+}
