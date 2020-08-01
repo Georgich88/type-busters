4ab904f1dd5307da1c47140c798cf965f5414e08
Georgich88 <georgeisaev@gmail.com>
2020-08-01 14:14:29 +0700

    Add component scan

--- exercises/src/main/java/io/typebusters/exercises/ExercisesApplication.java
+++ exercises/src/main/java/io/typebusters/exercises/ExercisesApplication.java
@@ -2,10 +2,12 @@ package io.typebusters.exercises;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"io.typebusters.exercises.repositories"})
+@ComponentScan({"io.typebusters.exercises.components"})
 public class ExercisesApplication {
 
 	public static void main(String[] args) {
