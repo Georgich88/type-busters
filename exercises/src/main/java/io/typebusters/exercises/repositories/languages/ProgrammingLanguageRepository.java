3566c60b5c892dc65f04270bd284edc3c29e62b4
Georgich88 <georgeisaev@gmail.com>
2020-08-01 14:11:03 +0700

    Optimize import

--- exercises/src/main/java/io/typebusters/exercises/repositories/languages/ProgrammingLanguageRepository.java
+++ exercises/src/main/java/io/typebusters/exercises/repositories/languages/ProgrammingLanguageRepository.java
@@ -2,7 +2,6 @@ package io.typebusters.exercises.repositories.languages;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import io.typebusters.exercises.model.exercises.Course;
 import io.typebusters.exercises.model.languages.ProgrammingLanguage;
 
 public interface ProgrammingLanguageRepository extends MongoRepository<ProgrammingLanguage, String> {
