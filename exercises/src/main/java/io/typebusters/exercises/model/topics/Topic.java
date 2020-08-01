4367e35b940448a2a5fe692689e84f327c68d48e
Georgich88 <georgeisaev@gmail.com>
2020-08-01 10:27:49 +0700

    Topic model: add object inherited methods and serializable

--- exercises/src/main/java/io/typebusters/exercises/model/topics/Topic.java
+++ exercises/src/main/java/io/typebusters/exercises/model/topics/Topic.java
@@ -1,11 +1,15 @@
 package io.typebusters.exercises.model.topics;
 
+import java.io.Serializable;
+
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.data.annotation.Id;
 
-public class Topic {
+public class Topic implements TopicUnderStudy, Serializable {
 
+	private static final long serialVersionUID = -3690357397730716762L;
+	
 	@Id
 	@NotBlank
 	private String id;
@@ -40,6 +44,42 @@ public class Topic {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	// Object inherited method
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder
+			.append("Topic [id=").append(id)
+			.append(", name=").append(name).append("]");
+		return builder.toString();
 	}
 	
 	
