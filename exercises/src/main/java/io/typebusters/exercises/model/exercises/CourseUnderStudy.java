626fd28e49d8bfea93498cae12f43ad0afc68190
Georgich88 <georgeisaev@gmail.com>
2020-09-24 08:37:26 +0700

    course implementation

--- exercises/src/main/java/io/typebusters/exercises/model/exercises/CourseUnderStudy.java
+++ exercises/src/main/java/io/typebusters/exercises/model/exercises/CourseUnderStudy.java
@@ -1,5 +1,10 @@
 package io.typebusters.exercises.model.exercises;
 
-public interface CourseUnderStudy {
+public interface CourseUnderStudy<T> {
 
+	T getId();
+
+	ExerciseUnderStudy<T> addExercise(T id, String text);
+
+	boolean deleteExercise(T id);
 }
